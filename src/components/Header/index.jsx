e5a7eccd9ee0ee28bfba36f96f1af1a6689c29bb
Michael Matos <mikematos84@gmail.com>
2025-05-19 09:23:35 -0400

    feat: basic css variables setups and header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="content">
+        <div className="title">Shopping List</div>
+      </div>
+    </header>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
